6a78801925114268f4a31aa765e18fa00fd71a25
smikhalevski <smikhalevski@gmail.com>
2023-09-04 21:18:02 +0300

    Better naming

--- src/scripts/gen-language-trie.js
+++ src/scripts/gen-language-trie.js
@@ -3,28 +3,28 @@ const path = require('path');
 const languages = require('./languages.json');
 const { createTrie, setValue } = require('@smikhalevski/trie');
 
-const languagesTrie = createTrie();
+const trie = createTrie();
 
 for (const [iso639x, iso6391] of Object.entries(languages)) {
-  setValue(languagesTrie, iso639x, iso6391);
+  setValue(trie, iso639x, iso6391);
 }
 
-function genTrieSrc(trie, index) {
+function genTrieSrc(node, index) {
   let src = '';
 
-  for (let charCode in trie) {
+  for (let charCode in node) {
     if ((charCode = +charCode) === charCode) {
-      src += `c${index}===${charCode}?${genTrieSrc(trie[charCode], index + 1)}:`;
+      src += `c${index}===${charCode}?${genTrieSrc(node[charCode], index + 1)}:`;
     }
   }
 
-  if (trie.isLeaf) {
-    if (trie.leafCharCodes) {
+  if (node.isLeaf) {
+    if (node.leafCharCodes) {
       src +=
-        trie.leafCharCodes.map((charCode, offset) => `c${index + offset}===${charCode}`).join('&&') +
-        `?'${trie.value}':0`;
+        node.leafCharCodes.map((charCode, offset) => `c${index + offset}===${charCode}`).join('&&') +
+        `?'${node.value}':0`;
     } else {
-      src += `'${trie.value}'`;
+      src += `'${node.value}'`;
     }
   } else {
     src += '0';
@@ -38,7 +38,7 @@ export default function(str: string, offset: number): string | 0 {
   const c0 = lowerCharCodeAt(str, offset);
   const c1 = lowerCharCodeAt(str, offset + 1);
   const c2 = lowerCharCodeAt(str, offset + 2);
-  return ${genTrieSrc(languagesTrie, 0)};
+  return ${genTrieSrc(trie, 0)};
 }`;
 
 const dir = path.resolve(__dirname, '../main/gen');
