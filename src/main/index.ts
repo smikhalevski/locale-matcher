30805895b7356195d61e37ca4e204b9bc56da8e0
smikhalevski <smikhalevski@gmail.com>
2025-10-18 23:17:23 +0300

    Removed redundant export

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,3 @@
 export { matchLocale } from './matchLocale.js';
-export { matchLocaleOrLanguage } from './matchLocaleOrLanguage.js';
 export { normalizeLocale } from './normalizeLocale.js';
 export { pickLocale } from './pickLocale.js';
