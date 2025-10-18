5141e44707429665c5d8b07e7ebe9c324906ca62
smikhalevski <smikhalevski@gmail.com>
2025-10-18 22:45:50 +0300

    Updated README.md

--- src/test/pickLocale.test.ts
+++ src/test/pickLocale.test.ts
@@ -1,13 +1,16 @@
-import { describe, test, expect } from 'vitest';
+import { expect, test } from 'vitest';
 import { pickLocale } from '../main/index.js';
 
-describe('pickLocale', () => {
-  test('matches a locale', () => {
-    expect(pickLocale('en', ['en_US', 'en', 'ru'])).toBe('en');
-    expect(pickLocale('en_GB', ['en_US', 'en_GB', 'ru'])).toBe('en_GB');
-  });
+test('matches a locale', () => {
+  expect(pickLocale('en', ['en_US', 'en', 'ru'])).toBe('en');
+  expect(pickLocale('en_GB', ['en_US', 'en_GB', 'ru'])).toBe('en_GB');
+});
 
-  test('returns the default locale if no locale matched', () => {
-    expect(pickLocale('cz_CZ', ['pt', 'ru'], 'zu')).toBe('zu');
-  });
+test('returns the default locale if no locale matched', () => {
+  expect(pickLocale('cz_CZ', ['pt', 'ru'], 'zu')).toBe('zu');
+});
+
+test('returns the shortest locale', () => {
+  expect(pickLocale(['en_US', 'en_AU'], ['en-AU', 'en-GB', 'en', 'ru'])).toBe('en');
+  expect(pickLocale('aaa-bbb-ccc', ['aaa', 'aaa-bbb', 'aaa-ddd'])).toBe('aaa-bbb');
 });
